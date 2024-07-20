27237502228885adb60683415e6761158702fb1f
Swapnil Jikar <112884653+WizKnight@users.noreply.github.com>
2024-07-21 02:51:16 +0530

    Update app.py, requirements.txt

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from transformers import pipeline, AutoModelForSeq2SeqLM,AutoTokenizer
 app = FastAPI()
 
 # Load model and tokenizer
-model_path = "E:\Git Uploads\English-to-Spanish-Translation-APP\notebook\results\checkpoint-28041"
+model_path = ".\models\model.safetensors"
 model = AutoModelForSeq2SeqLM.from_pretrained(model_path).to("cuda")  
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
@@ -32,3 +32,22 @@ async def translate_text(request: Request):
     translation = translator(text_to_translate)[0]["translation_text"]
     return templates.TemplateResponse("index.html", {"request": request, "translation": translation})
 
+
+import gradio as gr
+
+# Function to translate text
+def translate(text):
+    translated_text = translator(text)[0]["translation_text"]
+    return translated_text
+
+# Create a Gradio interface
+iface = gr.Interface(
+    fn=translate,
+    inputs=gr.Textbox(label="Enter English Text:"),
+    outputs=gr.Textbox(label="Spanish Translation:"),
+    title="English to Spanish Translator",
+    description="Enter English text to get the Spanish translation using a fine-tuned MarianMT model.",
+)
+
+# Launch the app within Spaces
+iface.launch()
